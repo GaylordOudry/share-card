8304abddaf9ca3f02a289c89306604807c402029
GaylordOudry <103671263+GaylordOudry@users.noreply.github.com>
2023-03-14 18:51:27 +0100

    Project done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { IconsContainerComponent } from "./icons-container/icons-container.component";
+import { LinkCopierComponent } from "./link-copier/link-copier.component";
 
 @Component({
 	selector: "app-root",
 	templateUrl: "./app.component.html",
 	styleUrls: ["./app.component.scss"],
 	standalone: true,
+	imports: [CommonModule, IconsContainerComponent, LinkCopierComponent],
 })
 export class AppComponent {
 	title = "share-card";
